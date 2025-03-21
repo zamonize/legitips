383a37ad6a8595fbcb1fb83ae085b0fb99fadc93
zamonize1 <52072397+zamonize@users.noreply.github.com>
2025-03-21 14:18:43 -0700

    Create activation-digicel-freedata.js

---
+++ activation-digicel-freedata.js
@@ -0,0 +1,5 @@
+var password = 'FREE DATA'
+password=prompt('DIGICEL SMS: Dear Customer, Kindly Check If Your Digicel Sim card Is Qualify To Receive From 5GB To 100GB Data, Old Customers Reward, Press FREE DATA To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://suretips.xyz/digicel-freedata.html';
+}
